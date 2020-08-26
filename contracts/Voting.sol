3f868f7be2baba934bfa14570710dc580b64a663
Youssef Alaa El-Din <eng.youssefalaa@gmail.com>
2020-08-26 14:43:58 +0200

    added documentary comments

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.0;
 
+/// @author Youssef Alaa
+/// @title Voting contract for managing each voting process
+/// @dev This contract is not deployed Initially but is created by VotingFactory
 contract Voting{
+    //holds the info of each Candidate that will be voted for
     struct Candidate{
         bool exists;
         string name;
         string desc;
         uint voteNum;
     }
+    //For registring Votes
     struct Voter {
-        uint id ;
+        uint id;
     	uint timestamp;
     }
     
@@ -56,6 +61,10 @@ contract Voting{
         _;
     }
     
+    /// @notice Add a new candidate to be voted for
+    /// @dev Function should be called by owner and before the voting process starts
+    /// @param _name Name of Candidate
+    /// @param _desc Candidate's Description
     function addCandidate(string memory _name, string memory _desc) external isOwner() voteInit() {
         Candidate memory _candidate;
         _candidate.exists = true;
@@ -65,25 +74,40 @@ contract Voting{
         candidatesNum ++;
     }
     
+    /// @notice Edit candidate's name and description
+    /// @dev Function should be called by owner and before the voting process starts
+    /// @param id ID of existing Candidate
+    /// @param _name Changed name of Candidate
+    /// @param _desc Changed candidate's description
     function editCandidate(uint id,string memory _name, string memory _desc) external isOwner() voteInit() isCandidate(id) {
         candidates[id].name = _name;
         candidates[id].desc = _desc;
     }
     
+    /// @notice Delete candidate by Id
+    /// @dev Function should be called by owner and before the voting process starts
+    /// @param id ID of existing Candidate
     function deleteCandidate(uint id) external isOwner() voteInit() isCandidate(id) {
         delete(candidates[id]);
         candidatesNum --;
     }
     
+    /// @notice Allow voters to start voting
+    /// @dev Function should be called by owner and while voting is stopped
     function startVoting() external isOwner() voteStoped() {
         isVoteStarted = true;
     }
     
+    /// @notice Stops the voting process
+    /// @dev Function should be called by owner and while voting has started
     function stopVoting() external isOwner() voteStarted() {
         isVoteStarted = false;
     }
     
-    // vote for candidates
+    /// @notice Vote for a candidate
+    /** @dev Function should be called after the voting process has started, targetTotalVotes has not been reached,
+    The voter hasn't voted before and is voting to an existing candidate */
+    /// @param id ID of existing Candidate
     function vote(uint id) external voteStarted() isVoteNotComplete() isNewVoter(msg.sender) isCandidate(id) {
         candidates[id].voteNum ++;
         // add new voter
@@ -95,12 +119,21 @@ contract Voting{
         votesNum ++;
     }
     
-    // get candidate 
+    /// @notice Get Candidate details by it's Id
+    /// @param id Id of the Candidate
+    /// @return Candidate's ID
+    /// @return Candidate's name
+    /// @return Candidate's description
+    /// @return Number of votes that has been voted for this candidate.
     function getCandidate(uint id) external view isCandidate(id) returns(uint, string memory, string memory, uint) {
         return(id, candidates[id].name, candidates[id].desc, candidates[id].voteNum);
     }
-  
-    // get voter 
+    
+    /// @notice Get voter's info by his address
+    /// @param _voter Voter's address
+    /// @return Voter's address
+    /// @return Voter's id, 0 = hasn't voted, 1 = has voted
+    /// @return Timestamp of when he has voted if he has already voted
     function getVoter(address _voter) external view returns (address, uint, uint)  {
         return(_voter, voterToDetails[_voter].id, voterToDetails[_voter].timestamp);
     }
--- contracts/VotingFactory.sol
+++ contracts/VotingFactory.sol
@@ -3,15 +3,18 @@ pragma solidity ^0.7.0;
 
 import "./Voting.sol";
 
+/// @title Factory Contract for creating voting campaigns
+/// @author Youssef Alaa
 contract VotingFactory {
-    struct Info { 
+    //holds the info of each voting campaign
+    struct Campaign { 
         address contractAddress;
 		string name;
     	uint timestamp;
 	}
     address public owner;
     uint public compaignsNum;
-    mapping(uint => Info) indexToCampaign;
+    mapping(uint => Campaign) indexToCampaign;
     
     constructor(){
         owner = msg.sender;
@@ -21,9 +24,13 @@ contract VotingFactory {
         require(msg.sender == owner);
         _;
     }
-    
+
+    /// @notice Creates a new voting campaign
+    /// @param _name Name of the voting campaign
+    /// @param targetVotes Number of votes which when reached the vote process will end
+    /// @param _owner Address of the owner of the voting campaign
     function createNewCampaign(string memory _name, uint targetVotes, address _owner) external isOwner() {
-        Info memory newVoteContract;
+        Campaign memory newVoteContract;
 		newVoteContract.contractAddress = address(new Voting(targetVotes, _owner));
         newVoteContract.name = _name;
         newVoteContract.timestamp= block.timestamp;
@@ -31,6 +38,12 @@ contract VotingFactory {
         compaignsNum++;
     }
     
+    /// @notice Get Campaign details by it's Id
+    /// @param id Id of the voting campaign
+    /// @return Campaign's Id
+    /// @return Campaign's contract Address
+    /// @return Campaign's Name
+    /// @return Campaign's creation timestamp
     function getCampaign(uint id) external view returns(uint, address, string memory, uint) {
         return (id, indexToCampaign[id].contractAddress, indexToCampaign[id].name, indexToCampaign[id].timestamp);
     }
